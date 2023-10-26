d1e8a50253392ca179375872d3e4a5d8b48515e3
zamieabdullah <zamieabdullah0@gmail.com>
2023-10-26 13:26:50 -0400

    creating the base for react and flask

---
+++ server/app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+# from flask_migrate import Migrate
+from flask_cors import CORS
+# from models.model import db
+# import os
+
+# importing environment variables 
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# importing models
+# from models.users import User
+
+# importing routes
+# from controllers.users import bp as user_routes
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Configure PostgreSQL connection
+# app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@" \
+                                        # f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+# db.init_app(app)
+# migrate = Migrate(app, db)
+
+# Create the database tables (if they don't exist)
+# with app.app_context():
+    # db.create_all()
+
+
+# Register the routes blueprint
+# app.register_blueprint(user_routes)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+if __name__ == '__main__':
+    app.run()
